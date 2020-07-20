d7b5b8a61e973bf8345f5d8257aacce944c8023f
Seungjae Park <astro.psj@gmail.com>
2020-07-20 21:25:51 +0900

    Update package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,14 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
     es6: true,
     node: true,
   },
   extends: [
-    'airbnb-base',
-    'plugin:@typescript-eslint/recommended',
+    'airbnb-typescript/base',
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.json',
-  },
-  plugins: ['@typescript-eslint'],
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-    },
-  },
-  rules: {
-    'import/extensions': ['error', 'ignorePackages', {
-      js: 'never',
-      jsx: 'never',
-      mjs: 'never',
-      ts: 'never',
-      tsx: 'never',
-    }],
   },
 };
